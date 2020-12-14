0c5cdafc4fa53d5149714ac1999d4b581d295841
Vitaliy Koren <vitaly@simporter.com>
2020-12-14 22:53:37 +0200

    first init

--- download.sh
+++ download.sh
@@ -9,19 +9,19 @@ Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 """
 
 URL=https://www.dropbox.com/s/tjxpypwpt38926e/wing.ckpt?dl=0
-mkdir -p ./checkpoints/
-OUT_FILE=./checkpoints/wing.ckpt
+mkdir -p ./app/checkpoints/
+OUT_FILE=./app/checkpoints/wing.ckpt
 wget -N $URL -O $OUT_FILE
 URL=https://www.dropbox.com/s/91fth49gyb7xksk/celeba_lm_mean.npz?dl=0
-OUT_FILE=./checkpoints/celeba_lm_mean.npz
+OUT_FILE=./app/checkpoints/celeba_lm_mean.npz
 wget -N $URL -O $OUT_FILE
 
 URL=https://www.dropbox.com/s/96fmei6c93o8b8t/100000_nets_ema.ckpt?dl=0
-mkdir -p ./checkpoints/celeba_hq
-OUT_FILE=./checkpoints/celeba_hq/100000_nets_ema.ckpt
+mkdir -p ./app/checkpoints/celeba_hq
+OUT_FILE=./app/checkpoints/celeba_hq/100000_nets_ema.ckpt
 wget -N $URL -O $OUT_FILE
 
 URL=https://www.dropbox.com/s/etwm810v25h42sn/100000_nets_ema.ckpt?dl=0
-mkdir -p ./checkpoints/afhq
-OUT_FILE=./checkpoints/afhq/100000_nets_ema.ckpt
+mkdir -p ./app/checkpoints/afhq
+OUT_FILE=./app/checkpoints/afhq/100000_nets_ema.ckpt
 wget -N $URL -O $OUT_FILE
